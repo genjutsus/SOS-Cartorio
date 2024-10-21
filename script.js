d9620e3099b2c5692016080ce41a0b76952e83aa
genjutsus <154602378+genjutsus@users.noreply.github.com>
2024-10-21 00:10:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,81 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const forms = {
         form1: `
-            <span color="red"><strong>REITERE QUE TODAS AS INFORMAÇÕES SÃO IMPORTANTES. CASO HAJA IMPOSSIBILIDADE DE ARRECADAR ALGUMA DAS INFORMAÇÕES ABAIXO, DIGITE desconhecido E <u>REVISE OS RESULTADOS APÓS CLICAR EM ENVIAR</u></strong></span><br><form id="form1">
-                <label for="input1">Nome do Hospital/Órgão solicitante:</label>
-                <input type="text" id="input1" name="input1" placeholder="Hospital Municipal Miguel Couto" required><br>
+            <span color="red"><strong>APÓS CLICAR EM ENVIAR, A PEÇA SERÁ PRODUZIDA, BASTANDO COPIAR E COLAR NO SCO. SE NECESSÁRIO, FAÇA SUAS EDIÇÕES ANTES DE GRAVAR.</strong></span><br><form id="form1">
+                <label for="input1ima">Destinatário (nome da pessoa ou função):</label>
+                <input type="text" id="input1ima" name="input1ima" placeholder="Responsável pelo Condomínio Leblon Ocean" required><br>
+            <font size="10"><i>Na falta de uma forma melhor de se referir ao destinatário, utilize algo como "Responsável pelo edifício localizado na Rua Barão da Torre, 255"</i></font>
+                <br>
+                <label for="input2ima">Você deseja imagens de qual área específica? ("da calçada"/"da recepção"/"da portaria e do depósito"/etc)</label>
+                <input type="text" id="input2ima" name="input2ima" placeholder="da calçada" required><br>
                 
-                <label for="input2">Nome do Funcionário comunicante:</label>
-                <input type="text" id="input2" name="input2" placeholder="FULANA CICLANA DE TAL" required><br>
+                <label for="input3ima">Endereço sobre o qual se referem as imagens:</label>
+                <input type="text" id="input3ima" name="input3ima" placeholder="Rua Barão da Torre, 255" required><br>
                 
-                <label for="input3">CPF do Funcionário:</label>
-                <input type="text" id="input3" name="input3" placeholder="000.000.000-00" required><br>
+                <label for="input4ima">Momento inicial das imagens:</label>
+                <input type="text" id="input4ima" name="input4ima" placeholder="06h00 (21/10/2024)" required><br>
                 
-                <label for="input4">Nome do Falecido:</label>
-                <input type="text" id="input4" name="input4" placeholder="BELTRANO DE TAL" required><br>
-                
-                <label for="input5">Nacionalidade do Falecido:</label>
-                <input type="text" id="input5" name="input5" placeholder="brasileiro" required><br>
-                
-                <label for="input6">Falecido é natural de qual cidade?</label>
-                <input type="text" id="input6" name="input6" placeholder="Duque de Caxias" required><br>
-                
-                <label for="input7">Estado Civil do Falecido:</label>
-                <input type="text" id="input7" name="input7" placeholder="solteiro" required><br>
-                
-                <label for="input8">Data do Nascimento do Falecido:</label>
-                <input type="text" id="input8" name="input8" placeholder="00/00/0000" required><br>
-                
-                <label for="input9">Identidade (nº do doc/órgão) do Falecido:</label>
-                <input type="text" id="input9" name="input9" placeholder="000000000/DETRAN-RJ" required><br>
-                
-                <label for="input10">Endereço do Falecido:</label>
-                <input type="text" id="input10" name="input10" placeholder="Rua das Couves, 01, Taquara, Rio de Janeiro - RJ" required><br>
-                
-                <label for="input11">Data de Entrada no Hospital/Órgão solicitante:</label>
-                <input type="text" id="input11" name="input11" placeholder="00/00/0000" required><br>
-                
-                <label for="input12">Hora de Entrada no Hospital/Órgão solicitante:</label>
-                <input type="text" id="input12" name="input12" placeholder="00h00" required><br>
-                
-                <label for="input13">Número do BAM:</label>
-                <input type="text" id="input13" name="input13" placeholder="000000" required><br>
-                
-                <label for="input14">Número do Prontuário:</label>
-                <input type="text" id="input14" name="input14" placeholder="000000" required><br>
-                
-                <label for="input15">Motivo da Morte (em tese):</label>
-                <input type="text" id="input15" name="input15" placeholder="queda da própria altura" required><br>
-                
-                <label for="input16">Local do Motivo da Morte:</label>
-                <input type="text" id="input16" name="input16" placeholder="Rua das Couves, 01, Taquara, Rio de Janeiro - RJ" required><br>
-                
-                <label for="input17">Primeiro Hospital/Órgão em que o falecido deu entrada:</label>
-                <input type="text" id="input17" name="input17" placeholder="Hospital Municipal Miguel Couto" required><br>
-                
-                <label for="input18">Socorrido por? ("Meios próprios" ou "Nome, Documento (pessoa física)" ou "Ambulância, Identificação da Ambulância"</label>
-                <input type="text" id="input18" name="input18" placeholder="Ambulância SAMU, RT 103" required><br>
-                
-                <label for="input19">Data da Morte:</label>
-                <input type="text" id="input19" name="input19" placeholder="00/00/2020" required><br>
-                
-                <label for="input20">Horário da Morte:</label>
-                <input type="text" id="input20" name="input20" placeholder="00h00" required><br>
-                
-                <label for="input21">Nome do Médico que atestou a Morte:</label>
-                <input type="text" id="input21" name="input21" placeholder="JUCLANO DE TAL" required><br>
-                
-                <label for="input22">CRM do Médico que atestou a Morte:</label>
-                <input type="text" id="input22" name="input22" placeholder="00-00000-0" required><br>
-                
-                <label for="input23">Número da Guia:</label>
-                <input type="text" id="input23" name="input23" placeholder="0000000" required><br>
-                
-                <label for="input24">Nome de quem identificou o Falecido:</label>
-                <input type="text" id="input24" name="input24"  placeholder="ZUCLANO DE TAL" required><br>
-                
-                <label for="input25">Parentesco entre quem identificou e o Falecido (é o que da vítima?):</label>
-                <input type="text" id="input25" name="input25" placeholder="irmão" required><br>
+                <label for="input5ima">Momento final das imagens:</label>
+                <input type="text" id="input5ima" name="input5ima" placeholder="18h00 (21/10/2024)" required><br>
                 
                 <input type="submit" value="Enviar">
             </form>
@@ -149,8 +90,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const resultTemplates = {
         form1: {
-            text: '<strong> DINÂMICA DO FATO </strong> <br> <br> {input1}, através do funcionário {input2}, CPF {input3}, comunica o falecimento e solicitação a remoção de: {input4}, {input5}, natural de {input6}, {input7}, nascido em {input8}, identidade {input9}, com residência na {input10}. O falecido deu entrada no {input1} no dia {input11} às {input12} e foi gerado o número do BAM {input13}, prontuário {input14}. A morte, em tese, foi devido a {input15}, no local {input16}. O falecido deu entrada inicialmente no {input17}, através de {input18}. {input4} veio a óbito no dia {input19} às {input20}, atestado pelo médico {input21}, CRM {input22}. Guia {input23}. O cadáver foi identificado por {input24}, {input25} da vítima. <br> <br> <strong> TERMO DE DECLARAÇÃO </strong> <br> <br> QUE é funcionário de {input1}; QUE comunica o falecimento e solicitação a remoção de: {input4}, {input5}, natural de {input6}, {input7}, nascido em {input8}, identidade {input9}, com residência na {input10}; QUE o falecido deu entrada no {input1} no dia {input11} às {input12} e foi gerado o número do BAM {input13}, prontuário {input14}; QUE a morte, em tese, foi devido a {input15}, no local {input16}; QUE o falecido deu entrada inicialmente no {input17}, através de {input18}; QUE {input4} veio a óbito no dia {input19} às {input20}, atestado pelo médico {input21}, CRM {input22}; QUE o número da Guia é {input23}; QUE o cadáver foi identificado por {input24}, {input25} da vítima. E mais não disse.',
-            instructions: '_____________ <br> <br> <strong>Observações:</strong> <br> 1. O crime é "Verificação para Remoção de Óbito". A vítima é o falecido. O georreferenciamento do local do fato é o local do motivo da morte. <br> 2. Após o R.O e o Termo de Declaração, gere a peça "Guia de Remoção de Cadáver" <br> 3. Por fim, ligue para o número (21) 8596-9532 para acionar a remoção.'
+            text: '<strong> DINÂMICA DO FATO </strong> <br> <br> Prezado(a) {input1ima},<br><br>Venho através do presente, com fundamento no artigo 2°, §2°, da Lei n. 12.830/2013, e no artigo 6° do Código de Processo Penal, REQUISITAR, a fim de instruir o Procedimento Policial em epígrafe, que tem como objeto a apuração de crime insculpido no Código Penal:<br><br>1. Que sejam enviadas a esta unidade de polícia judiciária (14ª Delegacia de Polícia - Rua Humberto de Campos, 315, Leblon) as imagens gravadas por TODAS as câmeras de segurança que tenham imagens de câmera {input2ima}, em {input3ima}, referentes ao período entre as {input4ima} e as {input5ima}, devendo a mídia ser disponibilizada de maneira compatível com a reprodução imediata no sistema operacional Windows 10 com seus recursos nativos. Também deve ser possível identificar a localização referente a cada câmera.<br><br>2. Caso o sistema de monitoramento seja gerido exclusivamente por alguma outra empresa e, em virtude disso, não seja possível realizar o acesso às imagens, que seja disponibilizado nome, e-mail e telefone do responsável.<br><br>3. Que, nesta última hipótese, caso as imagens já tenham sido processadas pela empresa ou por algum técnico, sejam enviadas as seleções obtidas, bem como eventuais imagens dos possíveis autores.<br><br>Saliento que o prazo para cumprimento do ora requisitado é de três dias úteis, alertando o destinatário que a inobservância deste lapso temporal acarretará na prática do crime previsto no artigo 330 do Código Penal.<br><br>Att.',
+            instructions: '_____________ <br> <br> <strong>Observações:</strong> <br> 1. Você deve imprimir duas cópias e deixar na caixa destinada a intimações e ofícios a serem entregues. <br> 2. O prazo padrão da peça gerada é de três dias úteis. Quando for colar a peça no SCO, pode alterar manualmente esse prazo, em casos excepcionais.'
         },
         form2: {
             text: '<strong> DINÂMICA DO FATO </strong> <br> <br> Narra o comunicante-vítima, {2nome}, que no dia {2data}, hora {2hora}, estava no local {2local}, {2fazendo}, quando surgiu um indivíduo, {2acompanhado}, que estava {2meio}, e arrebatou o seu celular ({2modelo}, IMEI nº {2imei}) de suas mãos, evadindo-se logo após em direção a {2evadiu}. As características lembradas do indivíduo que arrebatou o celular eram: {2descricao}. O fato foi testemunhado por {2testemunhas}. O comunicante afirma que é {2probabilidade} que haja imagens de câmera sobre o ocorrido. <br> <br> <strong> TERMO DE DECLARAÇÃO </strong> <br> <br> QUE no dia {2data}, hora {2hora}, estava no local {2local}, {2fazendo}, quando surgiu um indivíduo, {2acompanhado}, que estava {2meio}, e arrebatou o seu celular ({2modelo}, IMEI nº {2imei}) de suas mãos, evadindo-se logo após em direção a {2evadiu}; QUE as características lembradas do indivíduo que arrebatou o celular eram: {2descricao}; QUE o fato foi testemunhado por {2testemunhas}; QUE é {2probabilidade} que haja imagens de câmera sobre o ocorrido. E mais não disse.',
